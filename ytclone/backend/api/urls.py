02772a10a1be9a599da1c34a282c898728560660
Ark123-git <ark62744@gmail.com>
2025-07-09 19:27:44 +0530

    Add .gitignore to ignore media folder

--- ytclone/backend/api/urls.py
+++ ytclone/backend/api/urls.py
@@ -1,25 +1,4 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import VideoViewSet, CategoryViewSet, register_user, login_user, toggle_like, toggle_watch_later, add_comment,get_watch_later
-# from .views import toggle_subscribe
-# from . import views
-# router = DefaultRouter()
-# router.register(r'videos', VideoViewSet)
-# router.register(r'categories', CategoryViewSet)
 
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('register/', register_user),
-#     path('login/', login_user),
-#     path('videos/<int:video_id>/like/', toggle_like),
-#     path('videos/<int:video_id>/watchlater/', toggle_watch_later),
-#     path('videos/<int:video_id>/comment/', add_comment),
-#     path('watchlater/', get_watch_later),
-    
-    
-#     path('videos/<int:video_id>/subscribe/', toggle_subscribe, name='toggle_subscribe'),
-#     path('subscribe/<int:video_id>/', views.toggle_subscribe, name='toggle_subscribe'),
-# ]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
